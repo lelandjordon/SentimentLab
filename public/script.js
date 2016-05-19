705cce54f2a0d2509bfa2c36cf1fab272341336f
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-19 09:54:07 -0400

    Added Index controller

--- public/script.js
+++ public/script.js
@@ -6,18 +6,34 @@
     "ui.router",
     "ngResource"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("Index", IndexCtrl);
   
   Router.$inject = ["$stateProvider"];
   function Router($stateProvider){
     $stateProvider
     .state("index", {
       url:          "/",
-      templateUrl:  "/assets/html/questions-index.html"
+      templateUrl:  "/assets/html/questions-index.html",
+      controller:   "Index",
+      controllerAs: "IndexVM"
     })
     .state("show",  {
       url:          "/:title",
       templateUrl:  "/assets/html/questions-show.html"
     });
+  }
+  
+  IndexCtrl.$inject = [];
+  function IndexCtrl(){
+    var vm        = this;
+    vm.questions  = [
+      {
+        text: "Test123"
+      },
+      {
+        text: "Test456"
+      }
+    ];
   }
 })();
