b8ab0c09f692e137464c38b3b4ed760107f342d1
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-19 09:32:04 -0400

    Express renders questions as JSON

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require("express");
+var mongoose= require("./db/connection");
 
 var app     = express();
+var Question= mongoose.model("Question");
 
 app.get("/", function(req, res){
   res.send("This is working!");
+});
+
+app.get("/api/questions", function(req, res){
+  Question.find().then(function(questions){
+    res.json(questions);
+  });
 });
 
 app.listen(3001, function(){
